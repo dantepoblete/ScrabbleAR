37fcbdd04d561cb45c416d2ddb8e96c92e6bbfff
dantepoblete <62101433+dantepoblete@users.noreply.github.com>
2020-06-16 22:56:27 -0300

    Pruebas del uso de fichas

--- Tablero.py
+++ Tablero.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 from Bolsa import Bolsa
 from Ficha import Ficha
+from BackEnd import BackEnd
 
 #---------Ubicacion de las imagenes---------#
 
@@ -14,6 +15,8 @@ unknown='./letras/NN.png' #Casillas ocultas del atril de la CPU.
 
 cantX=cantY=15
 cantFichas=7
+
+letras=['A','B','C','D','E','F','G','H','I','J','K','LL','M','N','O','P','Q','R','RR','S','T','U','V','W','X','Y','Z']
 
 #---------Posicion de los casilleros especiales---------#
 
@@ -29,7 +32,11 @@ bolsa=Bolsa()
 bolsa.inicializar_Bolsa()
 fichas=bolsa.getFichas()
 
+tabla=BackEnd()
+
 #---------Tablero---------#
+
+claves=['DL','TL','DP','TP','N']
 
 def asignarImagen(i,j):
 	if((i,j)in posDobleLetra):
@@ -47,16 +54,16 @@ def asignarImagen(i,j):
 
 def asignarClave(i,j):
 	if((i,j)in posDobleLetra):
-		return 'DL'
+		return claves[0]
 	elif((i,j)in posTripleLetra):
-		return 'TL'
+		return claves[1]
 	elif((i,j)in posDoblePalabra):
-		return 'DP'
+		return claves[2]
 	elif((i,j)in posTriplePalabra):
-		return 'TP'
+		return claves[3]
 	else:
-		return 'N'
-
+		return claves[4]
+		
 def agregarDescripcion(i,j):
 	if((i,j)in posDobleLetra):
 		return 'Duplica el valor de tu letra'
@@ -65,14 +72,14 @@ def agregarDescripcion(i,j):
 	elif((i,j)in posDoblePalabra):
 		return 'Duplica el valor de tu palabra'
 	elif((i,j)in posTriplePalabra):
-		return 'Triplica el valor de tu palabra'
+		return 'Triplca el valor de tu palabra'
 	elif((i,j)in posInicial):
 		return 'Coloca una ficha aqui para comenzar el juego'
 	else:	
-		return None		
-										
+		return None	
+													
 
-tablero=[[sg.Button(tooltip=agregarDescripcion(i,j), image_filename=asignarImagen(i,j), image_size=(30,30), key=asignarClave(i,j), pad=(0,0)) for j in range(cantX)] for i in range(cantY)]
+tablero=[[sg.Button(tooltip=agregarDescripcion(i,j), image_filename=asignarImagen(i,j), key=(i,j), image_size=(30,30), pad=(0,0)) for j in range(cantX)] for i in range(cantY)]
 	
 #---------Atril CPU---------#	
 
@@ -80,17 +87,32 @@ atrilCPU=[sg.Button(image_filename=unknown, image_size=(30,30),pad=(0,0)) for i 
 
 #---------Atril Jugador---------#
 
-atrilJugador=[sg.Button(image_filename=fichas[i].getImage(), image_size=(30,30),key=(fichas[i].getLetra(),fichas[i].getValor()), pad=(0,0)) for i in range(cantFichas)]
+atrilJugador=[sg.Button(image_filename=fichas[i].getImage(), image_size=(30,30),key=(fichas[i].getLetra(),fichas[i].getValor(),i), pad=(0,0)) for i in range(cantFichas)]
 
 #---------Ventana---------#
 Interfaz=[[sg.Text('Atril CPU ')],
-	   atrilCPU,
-           [sg.Text('Tablero')],
+		atrilCPU,
            [sg.Column(tablero)],
            [sg.Text('Atril Jugador ')],
-	   atrilJugador
+		atrilJugador
       ]
-window = sg.Window('Scrabble').Layout(Interfaz) 
-event,values=window.Read()
+window = sg.Window('Scrabble').Layout(Interfaz)
+while True:
+	try:
+		event,values=window.Read()
+		if(event[0] in range(15)):
+			if((tabla.comprobarCasilla(event[0],event[1]))==True)and(aux!=None): #Compruebo si la casilla esta disponible y si aux esta inicializado
+				window[event].update(image_filename=aux.getImage()) #Actualizo la imagen de la casilla
+				tabla.actualizarCasilla(event[0],event[1],aux.getLetra(),aux.getValor()) #Actualizo los valores del BackEnd del tablero
+				window[clave].update(visible=False)  #Borro la ficha del atril
+				aux=None #Reseteo aux
+			elif(aux==None):
+				pass
+		elif(event[0] in letras):
+			aux=Ficha(event[0]) #Si se clickeo una ficha del atril, inicializo el objeto con su letra correspondiente
+			clave=event	#Guardo la clave de la ficha
+	except(NameError):
+		#Si el jugador hace click en el tablero antes de seleccionar una ficha, el programa no se cierra.
+		pass		
+
 window.Close()
-	
