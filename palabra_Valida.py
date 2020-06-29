355aff48e69d3de5c8c8c1e48d4500c98ee12fca
dantepoblete <62101433+dantepoblete@users.noreply.github.com>
2020-06-29 04:44:06 -0300

    Corregidos errores de identacion

--- palabra_Valida.py
+++ palabra_Valida.py
@@ -6,22 +6,21 @@ verb=[ "VAG", "VBG", "VAI","VAN", "MD", "VAS" , "VMG" , "VMI", "VB", "VMM" ,"VMN
 
 
 def clasificar(palabra):
-    pal = parse(palabra).split()
-    if pal in sus or adj or verb:
-         return True
-    else:
-        return False   
+	pal = parse(palabra).split()
+	if pal in sus or adj or verb:
+		return True
+	else:
+		return False   
    
 	
 def palabra_Valida(palabra):
-    if(((len(palabra))>=2) and ((len(palabra))<=7)) :
-         palabra=palabra.lower()
-         if (palabra in spelling):
-             return True
-         elif(palabra in lexicon):
-             return True
-     else:
-         return False        
-  
+	if(((len(palabra))>=2) and ((len(palabra))<=7)):
+		palabra=palabra.lower()
+		if (palabra in spelling):
+			return True
+		elif(palabra in lexicon):
+			return True
+	else:
+		return False
 
   
